1e93622dd977b9351a9a6bb65d73eefe73dac30b
Phil <Philip.Shaughnessy@gmail.com>
2017-01-30 13:32:23 -0500

    added initial choices

--- lipsum.rb
+++ lipsum.rb
@@ -1,4 +1,15 @@
 #A ruby file called lipsum.rb that can be run from the command line using ruby lipsum.rb <argument>.
 
 lipsum_wanted = ARGV[0]
-num = ARGV[1]
+num = ARGV[1] || 1
+
+case lipsum_wanted.downcase
+when "starwars" then puts "starwars"
+when "empire" then puts "empire"
+when "jedi" then puts "jedi"
+else
+  puts "Not an option. Choices are: \n
+    1) 'StarWars' for 'A New Hope' Lipsum.\n
+    2) 'Empire' for 'Empire Strikes Back' Lipsum.\n
+    3) 'Jedi' for 'Return of the Jedi' Lipsum."
+end
