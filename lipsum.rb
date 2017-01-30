2b4fb5053d19bddd8904ac27d81a6e032d7d4b5d
Phil <Philip.Shaughnessy@gmail.com>
2017-01-30 16:04:05 -0500

    removed a unhelpful comment

--- lipsum.rb
+++ lipsum.rb
@@ -2,10 +2,10 @@
 
 #A ruby file called lipsum.rb that can be run from the command line using ruby lipsum.rb <argument> <argument (optional)> to generate star wars based lipsum.
 #Second argument tells how many paragraphs to generate. Output is copied to clipboard.
-#Also can be run from [Directory]/lipsum.rb 
+#Also can be run from [Directory]/lipsum.rb
 
 lipsum_wanted = ARGV[0] || "none"
-ARGV[1].to_i.to_s == ARGV[1] ? num = ARGV[1].to_i : num = 1 if ARGV[1] #check if number
+ARGV[1].to_i.to_s == ARGV[1] ? num = ARGV[1].to_i : num = 1 if ARGV[1]
 
 
 starwars = "How did I get into this mess? I really don't know how. We seem to be made to suffer. It's our lot in life. I've got to rest before I fall apart. My joints are almost frozen. What a desolate place this is. Where are you going? Well, I'm not going that way. It's much too rocky. This way is much easier. What makes you think there are settlements over there? Don't get technical with me. What mission? What are you talking about? I've had just about enough of you! Go that way! You'll be malfunctioning within a day, you nearsighted scrap pile! And don't let me catch you following me begging for help, because you won't get it. No more adventures. I'm not going that way.\n"
