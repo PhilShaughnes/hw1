566ca9411864ccfb0c1848751df542b17e26bea0
Phil <Philip.Shaughnessy@gmail.com>
2017-01-31 09:23:08 -0500

    greatly simplified line 8 per Chris feedback.

--- lipsum.rb
+++ lipsum.rb
@@ -5,7 +5,7 @@
 # Also can be run from [Directory]/lipsum.rb
 
 lipsum_wanted = ARGV[0] || 'none'
-ARGV[1] && ARGV[1].to_i.to_s == ARGV[1] && ARGV[1].to_i > 1 ? num = ARGV[1].to_i : num = 1 
+ARGV[1].to_i > 1 ? num = ARGV[1].to_i : num = 1
 
 starwars = "How did I get into this mess? I really don't know how. We seem to be made to suffer. It's our lot in life. I've got to rest before I fall apart. My joints are almost frozen. What a desolate place this is. Where are you going? Well, I'm not going that way. It's much too rocky. This way is much easier. What makes you think there are settlements over there? Don't get technical with me. What mission? What are you talking about? I've had just about enough of you! Go that way! You'll be malfunctioning within a day, you nearsighted scrap pile! And don't let me catch you following me begging for help, because you won't get it. No more adventures. I'm not going that way.\n"
 
